564e1ddf428eb10e771a46b33ef98af9072a8386
luke-lite <105479902+luke-lite@users.noreply.github.com>
2024-04-02 11:33:15 -0400

    update

--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,6 @@ def index():
 
 @app.route('/upload_page', methods=['GET', 'POST'])
 def upload_page():
-
     form = UploadForm()
     if form.validate_on_submit():
         month = form.month.data
@@ -19,10 +18,15 @@ def upload_page():
         csv_file = form.csv_file.data
         filename = f"Crime_{month}_{year}.csv"  # Construct the filename
         upload_path = os.path.join('uploads', secure_filename(filename))
-        
-        if os.path.exists(upload_path):
-            # If file with the same name already exists, prompt user for confirmation
-            return render_template('upload_page.html', form=form, filename=filename, overwrite=True)
+
+        # confirmed = request.form.get('confirmed')
+        # if confirmed == 'true':
+        #     return render_template('index.html')
+
+        # if os.path.exists(upload_path):
+
+        #     # If file with the same name already exists, prompt user for confirmation
+        #     return render_template('upload_page.html', form=form, filename=filename, overwrite=True)
 
         # If file doesn't exist, or user confirms overwrite, save the file
         csv_file.save(upload_path)

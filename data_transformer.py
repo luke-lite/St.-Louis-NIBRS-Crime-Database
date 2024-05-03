616a67ea2e7abadc039d6318b1d362727f8a7a3e
luke-lite <105479902+luke-lite@users.noreply.github.com>
2024-05-03 16:02:49 -0400

    db testing

--- data_transformer.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import pandas as pd
-from utils import transform_data
-
-# Connect to SQLite database
-conn = sqlite3.connect('your_database.db')
-
-with conn:
-    transform_data(csv_loc, conn)
